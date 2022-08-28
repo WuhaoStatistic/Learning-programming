f8af95c1ba755c47ddc1f01c56e4671e860ed38b
WWuhao <89610539+WuhaoStatistic@users.noreply.github.com>
2022-08-28 16:45:18 +0200

    Add files via upload

---
+++ c++/引用做函数返回值.cpp
@@ -0,0 +1,28 @@
+﻿# include <iostream>
+# include "ListNode.h"
+# include <vector>
+# include <unordered_map>
+# include <string>
+# include <set>
+# include <math.h>
+# include <stack>
+# include <iterator>
+# include <algorithm>
+# include <queue>
+using namespace std;
+
+
+int& test()
+{
+	static int a = 20;
+	return a;
+}
+int main()
+{   
+	int &b = test();
+	cout << b;
+	test() = 1000;
+	cout << b;
+	return 0;
+}
+
