af7ba1eea6c16935673b65bda0fd6ba6c28db650
WWuhao <89610539+WuhaoStatistic@users.noreply.github.com>
2022-09-03 15:54:30 +0200

    Update and rename 函数调用重载.cpp to 函数调用重载与匿名对象.cpp

--- c++/函数调用重载与匿名对象.cpp
+++ c++/函数调用重载与匿名对象.cpp
@@ -16,11 +16,14 @@ class A {
 public:
     int m;
     int* n = new int(10);
+    A()
+    {
+    }
     A(int a)
     {
         this->m = a;
     }
-    //�º���functorû�й̶��ĸ�ʽ��һ�и�����Ҫ����ʵ��
+    //仿函数functor没有固定的格式，一切根据需要进行实现
     //
     void operator()(string test)
     {
@@ -30,8 +33,8 @@ public:
 
 
 
-//cout ȫ��ֻ����һ�� ����һ��Ҫ���ã�����ᴴ��һ���µ�
-// cout ����һ��Ҫ����cout����������������׷�� << ��
+//cout 全局只能有一个 这里一定要引用，否则会创建一个新的
+// cout 重载一定要返回cout对象，这样才能无限追加 << ，
 ostream& operator<<(ostream& out, A a)
 {
     out << "a.m is" << a.m;
@@ -42,8 +45,11 @@ ostream& operator<<(ostream& out, A a)
 int main()
 {
     A a1(1);
-    //�ڵ���ʱ���� ���󣨲�����������ʹ��
+    //在调用时就是 对象（参数）来进行使用
     a1("wuhao");
+    //匿名对象，在当前行使用完以后直接被释放，有点类似匿名函数
+    A()("abcd");
+    A(7)("sdsd");
     return 0;
 
 }
